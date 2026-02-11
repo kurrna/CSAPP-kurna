09e07b2cec3d804d19078336bad54f1c224f9a56
Kurna <kurna@foxmail.com>
2026-02-11 23:16:23 +0800

    bomb lab phase_4

--- lab/bomb/bomb.asm
+++ lab/bomb/bomb.asm
@@ -418,50 +418,55 @@ Disassembly of section .text:
   400fc9:	48 83 c4 18          	add    $0x18,%rsp                   # 恢复栈空间
   400fcd:	c3                   	retq                                # 返回    
 
+# 递归函数 func4
+# 在 0 和 14 之间执行二分查找
+# 若 arg1 = mid，则返回0
+# 若 arg1 < mid，则返回 2 * func4(arg1, arg2, mid - 1)
+# 若 arg1 > mid，则返回 2 * func4(arg1, mid + 1, arg3) + 1
 0000000000400fce <func4>:
   400fce:	48 83 ec 08          	sub    $0x8,%rsp
   400fd2:	89 d0                	mov    %edx,%eax
-  400fd4:	29 f0                	sub    %esi,%eax
+  400fd4:	29 f0                	sub    %esi,%eax                    # arg3 - arg2
   400fd6:	89 c1                	mov    %eax,%ecx
-  400fd8:	c1 e9 1f             	shr    $0x1f,%ecx
+  400fd8:	c1 e9 1f             	shr    $0x1f,%ecx                   
   400fdb:	01 c8                	add    %ecx,%eax
-  400fdd:	d1 f8                	sar    %eax
-  400fdf:	8d 0c 30             	lea    (%rax,%rsi,1),%ecx
-  400fe2:	39 f9                	cmp    %edi,%ecx
-  400fe4:	7e 0c                	jle    400ff2 <func4+0x24>
-  400fe6:	8d 51 ff             	lea    -0x1(%rcx),%edx
-  400fe9:	e8 e0 ff ff ff       	callq  400fce <func4>
-  400fee:	01 c0                	add    %eax,%eax
-  400ff0:	eb 15                	jmp    401007 <func4+0x39>
-  400ff2:	b8 00 00 00 00       	mov    $0x0,%eax
-  400ff7:	39 f9                	cmp    %edi,%ecx
-  400ff9:	7d 0c                	jge    401007 <func4+0x39>
-  400ffb:	8d 71 01             	lea    0x1(%rcx),%esi
-  400ffe:	e8 cb ff ff ff       	callq  400fce <func4>
-  401003:	8d 44 00 01          	lea    0x1(%rax,%rax,1),%eax
+  400fdd:	d1 f8                	sar    %eax                         # eax = (eax < 0) ? (eax - 1) / 2 : eax / 2
+  400fdf:	8d 0c 30             	lea    (%rax,%rsi,1),%ecx           # mid = arg2 + (arg3 - arg2) / 2
+  400fe2:	39 f9                	cmp    %edi,%ecx                    # 比较 arg1 和 arg2 + (arg3 - arg2) / 2
+  400fe4:	7e 0c                	jle    400ff2 <func4+0x24>          # 如果 arg1 >= mid 跳转到 0x400ff2
+  400fe6:	8d 51 ff             	lea    -0x1(%rcx),%edx              # arg3 = mid - 1
+  400fe9:	e8 e0 ff ff ff       	callq  400fce <func4>               # 当 arg1 < mid 时递归调用 func4
+  400fee:	01 c0                	add    %eax,%eax                    # eax = eax * 2
+  400ff0:	eb 15                	jmp    401007 <func4+0x39>          # 跳到 0x401007 结束
+  400ff2:	b8 00 00 00 00       	mov    $0x0,%eax                    # %eax = 0
+  400ff7:	39 f9                	cmp    %edi,%ecx                    # 比较 arg1 和 mid
+  400ff9:	7d 0c                	jge    401007 <func4+0x39>          # 如果 mid >= arg1 跳转到 0x401007
+  400ffb:	8d 71 01             	lea    0x1(%rcx),%esi               # arg2 = mid + 1
+  400ffe:	e8 cb ff ff ff       	callq  400fce <func4>               # 当 arg1 > mid 时递归调用 func4
+  401003:	8d 44 00 01          	lea    0x1(%rax,%rax,1),%eax        # eax = eax * 2 + 1
   401007:	48 83 c4 08          	add    $0x8,%rsp
   40100b:	c3                   	retq   
 
 000000000040100c <phase_4>:
   40100c:	48 83 ec 18          	sub    $0x18,%rsp
-  401010:	48 8d 4c 24 0c       	lea    0xc(%rsp),%rcx
-  401015:	48 8d 54 24 08       	lea    0x8(%rsp),%rdx
-  40101a:	be cf 25 40 00       	mov    $0x4025cf,%esi
+  401010:	48 8d 4c 24 0c       	lea    0xc(%rsp),%rcx               # %rcx = &arg2
+  401015:	48 8d 54 24 08       	lea    0x8(%rsp),%rdx               # %rdx = &arg1
+  40101a:	be cf 25 40 00       	mov    $0x4025cf,%esi               # "%d %d"
   40101f:	b8 00 00 00 00       	mov    $0x0,%eax
   401024:	e8 c7 fb ff ff       	callq  400bf0 <__isoc99_sscanf@plt>
-  401029:	83 f8 02             	cmp    $0x2,%eax
-  40102c:	75 07                	jne    401035 <phase_4+0x29>
-  40102e:	83 7c 24 08 0e       	cmpl   $0xe,0x8(%rsp)
-  401033:	76 05                	jbe    40103a <phase_4+0x2e>
+  401029:	83 f8 02             	cmp    $0x2,%eax                    # 检查是否成功读取两个整数  
+  40102c:	75 07                	jne    401035 <phase_4+0x29>        # 跳转到0x401035引爆
+  40102e:	83 7c 24 08 0e       	cmpl   $0xe,0x8(%rsp)               # 检查第一个输入是否小于等于14
+  401033:	76 05                	jbe    40103a <phase_4+0x2e>        # 小于等于则跳转0x40103a
   401035:	e8 00 04 00 00       	callq  40143a <explode_bomb>
-  40103a:	ba 0e 00 00 00       	mov    $0xe,%edx
-  40103f:	be 00 00 00 00       	mov    $0x0,%esi
-  401044:	8b 7c 24 08          	mov    0x8(%rsp),%edi
-  401048:	e8 81 ff ff ff       	callq  400fce <func4>
-  40104d:	85 c0                	test   %eax,%eax
-  40104f:	75 07                	jne    401058 <phase_4+0x4c>
+  40103a:	ba 0e 00 00 00       	mov    $0xe,%edx                    # %edx = 14
+  40103f:	be 00 00 00 00       	mov    $0x0,%esi                    # %esi = 0
+  401044:	8b 7c 24 08          	mov    0x8(%rsp),%edi               # %edi = arg1
+  401048:	e8 81 ff ff ff       	callq  400fce <func4>               # 调用 func4
+  40104d:	85 c0                	test   %eax,%eax                    # 返回值为0则标志位ZF设置为1
+  40104f:	75 07                	jne    401058 <phase_4+0x4c>        # 若返回值不为0（ZF为0）则跳转到0x401058引爆
   401051:	83 7c 24 0c 00       	cmpl   $0x0,0xc(%rsp)
-  401056:	74 05                	je     40105d <phase_4+0x51>
+  401056:	74 05                	je     40105d <phase_4+0x51>        # 若第二个输入为0则跳到0x40105d结束
   401058:	e8 dd 03 00 00       	callq  40143a <explode_bomb>
   40105d:	48 83 c4 18          	add    $0x18,%rsp
   401061:	c3                   	retq   
